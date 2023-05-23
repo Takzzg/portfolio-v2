9134645ac0061b3abaf47b52fb616c8fc65f4cf9
@Queiroz <guido.queiroz@front.id>
2023-05-23 04:22:08 -0300

    basic auth working

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
 import "../styles/common/globals.scss";
 import Layout from "../components/Layout/Layout";
 import LangProvider from "../context/Lang";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, session }: AppProps) {
 	return (
-		<LangProvider>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</LangProvider>
+		<SessionProvider session={session}>
+			<LangProvider>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</LangProvider>
+		</SessionProvider>
 	);
 }
 
---
+++ pages/auth/Login.tsx
@@ -0,0 +1,26 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import React from "react";
+
+type Props = {};
+
+const Login = (props: Props) => {
+	const { data: session } = useSession();
+
+	if (session) {
+		return (
+			<div>
+				<p>Welcome {session.user?.email}</p>
+				<button onClick={() => signOut()}>Sign Out</button>
+			</div>
+		);
+	}
+
+	return (
+		<div>
+			<p>Please log in</p>
+			<button onClick={() => signIn()}>Log In</button>
+		</div>
+	);
+};
+
+export default Login;
