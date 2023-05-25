6c6ffd4aa01d4ef25cfa45eed73675547732ff0d
@Queiroz <guido.queiroz@front.id>
2023-05-25 04:41:12 -0300

    fix login test 6

--- scripts/utilities/db/Users.ts
+++ scripts/utilities/db/Users.ts
@@ -1,24 +1,21 @@
 import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 export const onUserLogIn = async (userDetails: any) => {
 	// console.log("userDetails", userDetails);
-	const alreadyRegistered = await prisma.user.findUnique({
-		where: { id: userDetails.user.id },
-	});
-
-	if (alreadyRegistered) {
-		console.log("alreadyRegistered", alreadyRegistered);
-		return;
-	}
-
-	const { id, name, email, image } = userDetails.user;
-	const user = await prisma.user.create({
-		data: { id, name, email, image },
-	});
-	console.log("user", user);
-
-	return user;
+	// const alreadyRegistered = await prisma.user.findUnique({
+	// 	where: { id: userDetails.user.id },
+	// });
+	// if (alreadyRegistered) {
+	// 	console.log("alreadyRegistered", alreadyRegistered);
+	// 	return;
+	// }
+	// const { id, name, email, image } = userDetails.user;
+	// const user = await prisma.user.create({
+	// 	data: { id, name, email, image },
+	// });
+	// console.log("user", user);
+	// return user;
 };
