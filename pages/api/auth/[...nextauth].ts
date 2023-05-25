c286418c44936c0e89d66dc7bca8977207f4938b
@Queiroz <guido.queiroz@front.id>
2023-05-25 05:10:57 -0300

    fix login test 11

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,8 +6,6 @@ import { onUserLogIn } from "./_utilities";
 
 const prisma = new PrismaClient();
 
-// const onUserLogIn = (details: any) => {};
-
 export const authOptions: AuthOptions = {
 	adapter: PrismaAdapter(prisma),
 	providers: [
@@ -16,22 +14,22 @@ export const authOptions: AuthOptions = {
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
 	],
-	callbacks: {
-		async signIn(userDetails) {
-			// const user = await onUserLogIn(userDetails);
-			// console.log("user", user);
-			return true;
-		},
-		async redirect({ url, baseUrl }) {
-			return baseUrl;
-		},
-		async session({ session, user, token }) {
-			return session;
-		},
-		async jwt({ token, user, account, profile, isNewUser }) {
-			return token;
-		},
-	},
+	// callbacks: {
+	// 	async signIn(userDetails) {
+	// 		// const user = await onUserLogIn(userDetails);
+	// 		// console.log("user", user);
+	// 		return true;
+	// 	},
+	// 	async redirect({ url, baseUrl }) {
+	// 		return baseUrl;
+	// 	},
+	// 	async session({ session, user, token }) {
+	// 		return session;
+	// 	},
+	// 	async jwt({ token, user, account, profile, isNewUser }) {
+	// 		return token;
+	// 	},
+	// },
 	secret: process.env.NEXTAUTH_SECRET,
 };
 
