917b34d7449d0e05d73776b3dd7fa3d9e34aa70e
@Queiroz <guido.queiroz@front.id>
2023-05-25 05:40:11 -0300

    fix login test 16

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { onUserLogIn } from "./utilities";
+import { onUserLogIn } from "../../../public/utilities";
 
 const prisma = new PrismaClient();
 
