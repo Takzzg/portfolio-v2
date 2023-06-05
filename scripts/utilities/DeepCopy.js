7aedacc8fa299b96ce5dcc06786147777f7a4a49
@Queiroz <guido.queiroz@front.id>
2023-06-05 16:03:52 -0300

    vercel structuredClone fix 2

--- scripts/utilities/DeepCopy.js
+++ scripts/utilities/DeepCopy.js
@@ -1,7 +1,7 @@
 // "Middleware Hack"
 // https://github.com/vercel/next.js/discussions/33189#discussioncomment-3760898
 
-export const DeepCopy = (target: any) => {
+export const DeepCopy = (target) => {
 	const copy = structuredClone(target);
 	return copy;
 };
