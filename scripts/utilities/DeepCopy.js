f81fac3017b4b5f2b047c18f92ea081e56a10b62
@Queiroz <guido.queiroz@front.id>
2023-06-05 16:55:49 -0300

    remane old .js file

--- scripts/utilities/DeepCopy.js
+++ scripts/utilities/DeepCopy.js
@@ -1,5 +1,6 @@
 // "Middleware Hack"
 // https://github.com/vercel/next.js/discussions/33189#discussioncomment-3760898
+// seems that it needs to be a .js file in order for vercel to build successfully
 
 export const DeepCopy = (target) => {
 	const copy = structuredClone(target);
